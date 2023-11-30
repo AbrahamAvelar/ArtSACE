a9898d9b65bff5bfe9565c84f9e94e403b248820
AbrahamAvelar <jabrahamavelar@gmail.com>
2023-11-30 12:18:42 -0600

    Update Het_Script_CONConlySACE.sh

--- Het/Het_Script_CONConlySACE.sh
+++ Het/Het_Script_CONConlySACE.sh
@@ -7,9 +7,15 @@ printf "File,Strain,Total_Variants,Het_Count,Heterozygosity\n" > "$output_file"
 cut -d',' -f1 /mnt/Timina/lmorales/Public/ymez/tmp/06_genotyping/trees/SACE467/SampleSheet_SACE467_V8.csv | cat | while IFS= read -r element; do
 
 echo "Processing: $element"
-cp "/mnt/Timina/lmorales/Public/ymez/tmp/06_genotyping/${element}_CONC.gt.SNP_onlychr_SACE.g.vcf.gz" .
-gunzip "${element}_CONC.gt.SNP_onlychr_SACE.g.vcf.gz"
-vcf_file="${element}_CONC.gt.SNP_onlychr_SACE.g.vcf"
+#cp "/mnt/Timina/lmorales/Public/ymez/tmp/06_genotyping/${element}_CONC.gt.SNP_onlychr_SACE.g.vcf.gz" .
+#gunzip "${element}_CONC.gt.SNP_onlychr_SACE.g.vcf.gz"
+#vcf_file="${element}_CONC.gt.SNP_onlychr_SACE.g.vcf"
+
+cp "/mnt/Timina/lmorales/Public/ymez/tmp/06_genotyping/${element}_CONC.SNP_onlychr_SACE.g.vcf.gz" .
+cp "/mnt/Timina/lmorales/Public/ymez/tmp/06_genotyping/${element}_CONC.SNP_onlychr_SACE.g.vcf" .
+gunzip "${element}_CONC.SNP_onlychr_SACE.g.vcf.gz"
+vcf_file="${element}_CONC.SNP_onlychr_SACE.g.vcf"
+
 
 # Calculate heterozygosity and count heterozygous sites
 het_count=$(bcftools query -f '[%GT]\n' "$vcf_file" | awk '{het_count += gsub(/0\/1|1\/0/, "")} END {print het_count}')
